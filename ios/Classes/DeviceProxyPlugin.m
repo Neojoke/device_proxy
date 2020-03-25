b8758537c802bf5d4a48c699954090c357ad2570
wupeng01 <wupeng@ppdai.com>
2020-03-25 14:50:08 +0800

    删除swift 相关代码

--- ios/Classes/DeviceProxyPlugin.m
+++ ios/Classes/DeviceProxyPlugin.m
@@ -1,8 +1,31 @@
 #import "DeviceProxyPlugin.h"
-#import <device_proxy/device_proxy-Swift.h>
 
 @implementation DeviceProxyPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftDeviceProxyPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+        methodChannelWithName:@"com.intechlab/device_proxy"
+              binaryMessenger:[registrar messenger]];
+    DeviceProxyPlugin* instance = [[DeviceProxyPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getProxySetting" isEqualToString:call.method]) {
+    result([self getProxyUrl]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+- (NSString *)getProxyUrl{
+    NSDictionary *proxySettings = (__bridge NSDictionary *)CFNetworkCopySystemProxySettings();
+    NSArray *proxies = (__bridge NSArray *)CFNetworkCopyProxiesForURL((__bridge CFURLRef)[NSURL URLWithString:@"http://www.baidu.com"], (__bridge CFDictionaryRef)proxySettings);
+    NSDictionary *settings = [proxies objectAtIndex:0];
+    NSString * hostName = [settings objectForKey:(NSString *)kCFProxyHostNameKey];
+    NSString * protNumber = [settings objectForKey:(NSString *)kCFProxyPortNumberKey];
+    NSString * proxyType = [settings objectForKey:(NSString *)kCFProxyTypeKey];
+    NSLog(@"host=%@", hostName);
+    NSLog(@"port=%@", protNumber);
+    NSLog(@"type=%@", proxyType);
+    return [NSString stringWithFormat:@"%@:%@", hostName?hostName:@"", protNumber?protNumber:@""];
+
 }
 @end
